f634a539d43a97388d581f04e498e0b591416a4a
Haider  Tariq <haider.tariq@A006-01494.local>
2024-10-31 21:32:05 +0500

    Updates

---
+++ precache-manifest.e2b75732641ac59924a9fec95d753730.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ef236ae2fafaf00cb02ffa09cdfc6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f15e8a9bb10c297c73c",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "6e444546a56d46d811a9",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "3f15e8a9bb10c297c73c",
+    "url": "/static/js/2.410f4de7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.410f4de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e444546a56d46d811a9",
+    "url": "/static/js/main.45499261.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
